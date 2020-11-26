b4326d971a851568bc61374507c2e6aea1c1ab9f
dominicpaul17 <66663837+dominicpaul17@users.noreply.github.com>
2020-11-26 13:08:42 +0530

    Add files via upload

---
+++ day 4.py
@@ -0,0 +1,26 @@
+l= [1,2,3,4,5,6,7,8,9,10,99]
+l.append(12)
+print("add an item to the list")
+print(l)
+
+
+print("min value= a")
+a= min(l)
+print(a)
+print("max value =b")
+b = max(l)
+print(b)
+
+
+print("delete the list")
+del l
+
+
+
+x = ("python")
+y = reversed(x)
+print(tuple(y))
+
+tuple = ('python',123,'maths')
+list = list(tuple)
+print(list)
