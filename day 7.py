464c9f7ecef63a4a0b3ce490cbfaf0ce70d623a3
dominicpaul17 <66663837+dominicpaul17@users.noreply.github.com>
2020-11-29 20:40:07 +0530

    Add files via upload

---
+++ day 7.py
@@ -0,0 +1,18 @@
+from importfile import*
+import importfile as imp
+b =imp.list_imp()
+print("the list befor updation:",b)
+c = b
+c[3]=4
+c[4]=5
+print("the list after updation",c)
+import random as rnd
+d = rnd.randint(1,100)
+print(d)
+import sys
+print(sys.path)
+import pandas as pd
+
+
+
+
---
+++ importfile.py
@@ -0,0 +1,3 @@
+def list_imp():
+    a =[1,2,3,5,4]
+    return a
